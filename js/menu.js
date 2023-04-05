0470824b4d56b6475baea439ef848fdc74955b42
MartinG1997 <martingrinoh@gmail.com>
2023-04-05 08:58:04 -0400

    Navbar con JS terminado pero no pensado para ser responsive

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,27 @@
 const hamburguerIcon = document.querySelector('.nav-hamb');
+const navOverlay = document.querySelector('.nav-overlay')
 
 hamburguerIcon.addEventListener('click', ()=>{
     hamburguerIcon.classList.toggle('nav-hamb-op');
+
+    navOverlay.classList.toggle('nav-overlay-show');
+});
+
+navOverlay.addEventListener('click', (e)=>{
+    e.preventDefault();
+    const currentElement = e.target;
+    if(isActive(currentElement,'nav-parent')){
+        const subMenu = currentElement.parentElement.children[1];
+        if(window.innerWidth < 768){
+            let height = (subMenu.clientHeight == 0) ? subMenu.scrollHeight : 0;
+            subMenu.style.height = `${height}px`;
+        }
+        else{
+
+        }
+    };
 })
+
+function isActive(element, string){
+    return element.classList.value.includes(string);
+}
